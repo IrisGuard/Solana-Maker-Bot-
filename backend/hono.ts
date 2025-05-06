664dcdfdb0f5217cc32a567ac002f421f4820e4d
IrisGuard <infoirisguard@gmail.com>
2025-05-06 09:49:56 +0300

    Update hono.ts

--- backend/hono.ts
+++ backend/hono.ts
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV !== 'production') {
     fetch: app.fetch,
     port: 3000,
   });
-  console.log('Server is running on http://localhost:3000');
+  
 }
 
 export default app;
